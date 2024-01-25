093ebb1cec30c7afaf8864953eb188553aacae74
ronamit <ronamit@campus.technion.ac.il>
2024-01-25 14:17:13 +0000

    refactor

--- experiments/nyuv2/metrics.py
+++ experiments/nyuv2/metrics.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
-from sklearn.metrics import (accuracy_score, classification_report,
-                             confusion_matrix, multilabel_confusion_matrix)
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, multilabel_confusion_matrix
 from sklearn.preprocessing import MultiLabelBinarizer
 
 """Source: https://github.com/lorenmt/mtan"""
@@ -24,11 +23,11 @@ def clf_report(pred, target, main_task=None, stl=False):
         # so this is the same as taking the one with prob > .5
         return classification_report(target, pred, output_dict=True)
 
-    else:  # mtl
-        target = [[t] for t in target]
-        one_hot_target = MultiLabelBinarizer().fit_transform(target)
-        preds = (pred > 0) * 1  # this is equivalent to prob > .5
-        return classification_report(one_hot_target, preds, output_dict=True)
+    # mtl
+    target = [[t] for t in target]
+    one_hot_target = MultiLabelBinarizer().fit_transform(target)
+    preds = (pred > 0) * 1  # this is equivalent to prob > .5
+    return classification_report(one_hot_target, preds, output_dict=True)
 
 
 def compute_miou(x_pred, x_output, class_nb=13):
@@ -40,10 +39,12 @@ def compute_miou(x_pred, x_output, class_nb=13):
         first_switch = True
         for j in range(class_nb):
             pred_mask = torch.eq(
-                x_pred_label[i], j * torch.ones(x_pred_label[i].shape).type(torch.LongTensor).to(x_pred.device)
+                x_pred_label[i],
+                j * torch.ones(x_pred_label[i].shape).type(torch.LongTensor).to(x_pred.device),
             )
             true_mask = torch.eq(
-                x_output_label[i], j * torch.ones(x_output_label[i].shape).type(torch.LongTensor).to(x_pred.device)
+                x_output_label[i],
+                j * torch.ones(x_output_label[i].shape).type(torch.LongTensor).to(x_pred.device),
             )
             mask_comb = pred_mask.type(torch.FloatTensor) + true_mask.type(torch.FloatTensor)
             union = torch.sum((mask_comb > 0).type(torch.FloatTensor))
@@ -77,14 +78,14 @@ def compute_iou(x_pred, x_output):
         if i == 0:
             pixel_acc = torch.div(
                 torch.sum(torch.eq(x_pred_label[i], x_output_label[i]).type(torch.FloatTensor)),
-                torch.sum((x_output_label[i] >= 0).type(torch.FloatTensor))
+                torch.sum((x_output_label[i] >= 0).type(torch.FloatTensor)),
             )
         else:
             pixel_acc = pixel_acc + torch.div(
                 torch.sum(
-                    torch.eq(x_pred_label[i], x_output_label[i]).type(torch.FloatTensor)
+                    torch.eq(x_pred_label[i], x_output_label[i]).type(torch.FloatTensor),
                 ),
-                torch.sum((x_output_label[i] >= 0).type(torch.FloatTensor))
+                torch.sum((x_output_label[i] >= 0).type(torch.FloatTensor)),
             )
     return pixel_acc / batch_size
 
@@ -95,15 +96,21 @@ def depth_error(x_pred, x_output):
     x_output_true = x_output.masked_select(binary_mask)
     abs_err = torch.abs(x_pred_true - x_output_true)
     rel_err = torch.abs(x_pred_true - x_output_true) / x_output_true
-    return torch.sum(abs_err) / torch.nonzero(binary_mask).size(0), \
-        torch.sum(rel_err) / torch.nonzero(binary_mask).size(0)
+    return torch.sum(abs_err) / torch.nonzero(binary_mask).size(0), torch.sum(rel_err) / torch.nonzero(
+        binary_mask,
+    ).size(0)
 
 
 def normal_error(x_pred, x_output):
-    binary_mask = (torch.sum(x_output, dim=1) != 0)
-    error = torch.acos(
-        torch.clamp(torch.sum(x_pred * x_output, 1).masked_select(binary_mask), -1, 1)
-    ).detach().cpu().numpy()
+    binary_mask = torch.sum(x_output, dim=1) != 0
+    error = (
+        torch.acos(
+            torch.clamp(torch.sum(x_pred * x_output, 1).masked_select(binary_mask), -1, 1),
+        )
+        .detach()
+        .cpu()
+        .numpy()
+    )
     error = np.degrees(error)
     return np.mean(error), np.median(error), np.mean(error < 11.25), np.mean(error < 22.5), np.mean(error < 30)
 
@@ -116,8 +123,8 @@ def confusion_mat(pred, target, main_task=None, stl=False):
         # so this is the same as taking the one with prob > .5
         return confusion_matrix(target, pred)
 
-    else:  # mtl
-        target = [[t] for t in target]
-        one_hot_target = MultiLabelBinarizer().fit_transform(target)
-        preds = (pred > 0) * 1  # this is equivalent to prob > .5
-        return multilabel_confusion_matrix(one_hot_target, preds)
+    # mtl
+    target = [[t] for t in target]
+    one_hot_target = MultiLabelBinarizer().fit_transform(target)
+    preds = (pred > 0) * 1  # this is equivalent to prob > .5
+    return multilabel_confusion_matrix(one_hot_target, preds)
--- experiments/nyuv2/model.py
+++ experiments/nyuv2/model.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn as nn
+from torch import nn
 from torch.nn import functional as F
 
 
@@ -9,46 +9,57 @@ class SegNetSplit(nn.Module):
     SegNet with hard-parameter sharing
 
     """
+
     def __init__(self, logsigma=True):
         """
 
         :param logsigma: for uncert weighting
         """
         super().__init__()
-        filter = [64, 128, 256, 512, 1024]
+        channels = [64, 128, 256, 512, 1024]
         self.class_nb = 13
 
         # define encoder decoder layers
-        self.encoder_block = nn.ModuleList([self.conv_layer([3, filter[0]])])
-        self.decoder_block = nn.ModuleList([self.conv_layer([filter[0], filter[0]])])
+        self.encoder_block = nn.ModuleList([self.conv_layer([3, channels[0]])])
+        self.decoder_block = nn.ModuleList([self.conv_layer([channels[0], channels[0]])])
         for i in range(4):
-            self.encoder_block.append(self.conv_layer([filter[i], filter[i + 1]]))
-            self.decoder_block.append(self.conv_layer([filter[i + 1], filter[i]]))
+            self.encoder_block.append(self.conv_layer([channels[i], channels[i + 1]]))
+            self.decoder_block.append(self.conv_layer([channels[i + 1], channels[i]]))
 
         # define convolution layer
-        self.conv_block_enc = nn.ModuleList([self.conv_layer([filter[0], filter[0]])])
-        self.conv_block_dec = nn.ModuleList([self.conv_layer([filter[0], filter[0]])])
+        self.conv_block_enc = nn.ModuleList([self.conv_layer([channels[0], channels[0]])])
+        self.conv_block_dec = nn.ModuleList([self.conv_layer([channels[0], channels[0]])])
         for i in range(4):
             if i == 0:
-                self.conv_block_enc.append(self.conv_layer([filter[i + 1], filter[i + 1]]))
-                self.conv_block_dec.append(self.conv_layer([filter[i], filter[i]]))
+                self.conv_block_enc.append(self.conv_layer([channels[i + 1], channels[i + 1]]))
+                self.conv_block_dec.append(self.conv_layer([channels[i], channels[i]]))
             else:
-                self.conv_block_enc.append(nn.Sequential(self.conv_layer([filter[i + 1], filter[i + 1]]),
-                                                         self.conv_layer([filter[i + 1], filter[i + 1]])))
-                self.conv_block_dec.append(nn.Sequential(self.conv_layer([filter[i], filter[i]]),
-                                                         self.conv_layer([filter[i], filter[i]])))
+                self.conv_block_enc.append(
+                    nn.Sequential(
+                        self.conv_layer([channels[i + 1], channels[i + 1]]),
+                        self.conv_layer([channels[i + 1], channels[i + 1]]),
+                    ),
+                )
+                self.conv_block_dec.append(
+                    nn.Sequential(
+                        self.conv_layer([channels[i], channels[i]]),
+                        self.conv_layer([channels[i], channels[i]]),
+                    ),
+                )
 
         # define task specific layers
-        self.pred_task1 = nn.Sequential(nn.Conv2d(in_channels=filter[0], out_channels=filter[0], kernel_size=3,
-                                                  padding=1),
-                                        nn.Conv2d(in_channels=filter[0], out_channels=self.class_nb, kernel_size=1,
-                                                  padding=0))
-        self.pred_task2 = nn.Sequential(nn.Conv2d(in_channels=filter[0], out_channels=filter[0], kernel_size=3,
-                                                  padding=1),
-                                        nn.Conv2d(in_channels=filter[0], out_channels=1, kernel_size=1, padding=0))
-        self.pred_task3 = nn.Sequential(nn.Conv2d(in_channels=filter[0], out_channels=filter[0], kernel_size=3,
-                                                  padding=1),
-                                        nn.Conv2d(in_channels=filter[0], out_channels=3, kernel_size=1, padding=0))
+        self.pred_task1 = nn.Sequential(
+            nn.Conv2d(in_channels=channels[0], out_channels=channels[0], kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=channels[0], out_channels=self.class_nb, kernel_size=1, padding=0),
+        )
+        self.pred_task2 = nn.Sequential(
+            nn.Conv2d(in_channels=channels[0], out_channels=channels[0], kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=channels[0], out_channels=1, kernel_size=1, padding=0),
+        )
+        self.pred_task3 = nn.Sequential(
+            nn.Conv2d(in_channels=channels[0], out_channels=channels[0], kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=channels[0], out_channels=3, kernel_size=1, padding=0),
+        )
 
         # define pooling and unpooling functions
         self.down_sampling = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
@@ -74,7 +85,7 @@ class SegNetSplit(nn.Module):
         conv_block = nn.Sequential(
             nn.Conv2d(in_channels=channel[0], out_channels=channel[1], kernel_size=3, padding=1),
             nn.BatchNorm2d(num_features=channel[1]),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
         )
         return conv_block
 
@@ -114,5 +125,4 @@ class SegNetSplit(nn.Module):
 
         if self.logsigma is not None:
             return out, self.logsigma
-        else:
-            return out
+        return out
